6433caeba13604577784eba26d5eb781b97c834c
mirch <mirceateodor.oprea@gmail.com>
2024-02-19 14:19:42 +0200

    Enable relative line numbers

--- init.lua
+++ init.lua
@@ -300,8 +300,8 @@ require('lazy').setup({
 -- Set highlight on search
 vim.o.hlsearch = false
 
--- Make line numbers default
-vim.wo.number = true
+-- Make relative line numbers default
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
